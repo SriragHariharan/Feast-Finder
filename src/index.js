31c156fcd355e38914b8daa7449b826c0d284b6e
Srirag Hariharan <sriraghariharan108@gmail.com>
2023-10-01 10:25:45 +0530

    Created a basic react app from scratch and added parcel as a bundler.

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./app";
+
+//create root element
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+//rendering app componet in root
+root.render(<App />)
