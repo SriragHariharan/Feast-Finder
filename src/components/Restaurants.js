91c025499e1cc92da0f4ff2a33e3fdc8ace69ca9
Srirag Hariharan <sriraghariharan108@gmail.com>
2023-10-06 13:24:40 +0530

    Added search functionality and filter functionality based on user queries.

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -1,27 +1,48 @@
 //this component is used to map thru the restaurants fetched from api
 
+import { useEffect, useState } from "react";
 import useGetRestaurants from "../hooks/useGetRestaurants";
 import RestaurantCard from "./RestaurantCard";
+import Search from "./Search";
 import HomeShimmerLayout from "./shimmers/HomeShimmerLayout";
 
 const Restaurants = () => {
+    const [filteredRestaurants, setFilteredRestaurants] = useState([])
     const restaurants = useGetRestaurants();
+    // console.log("first ::", restaurants)
     
+    useEffect(() => {
+        setFilteredRestaurants(restaurants)
+    },[restaurants])
+
     //shimmer code to be shown on loading
     if(restaurants.length === 0){
         return <HomeShimmerLayout />
     }
 
+    //update filtered restaurants
+      const updateFilteredRestaurants = (newRestaurants) => {
+        setFilteredRestaurants(newRestaurants);
+    };
+
     return(
-        <div className="p-7 sm:p-20 grid grid-cols-4 gap-6">
+        <>
+            <Search restaurants={restaurants} updateFilteredRestaurants={updateFilteredRestaurants} />
             {
-                restaurants.map(res =>(
-                    <div key={res?.info?.id} className="col-span-2 sm:col-span-2 md:col-span-2 lg:col-span-1">
-                        <RestaurantCard restaurantInfo={res} />
-                    </div>
-                ) )
-            }            
-        </div>
+                filteredRestaurants?.length ===0 ?
+                (<div className="text-center text-blue-700 mt-32 mb-32 font-bold text-3xl">No Restaurants to show</div>)
+                :
+                (<div className="p-7 sm:p-20 grid grid-cols-4 gap-6">
+                    {
+                        filteredRestaurants.map(res =>(
+                            <div key={res?.info?.id} className="col-span-2 sm:col-span-2 md:col-span-2 lg:col-span-1">
+                                <RestaurantCard restaurantInfo={res} />
+                            </div>
+                        ) )
+                    }            
+                </div>)
+            }
+        </>
     )
 }
 
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,21 +1,46 @@
+import { useState } from "react";
 
-function Search() {
+function Search({ restaurants, updateFilteredRestaurants }) {
+
+    const [query, setQuery] = useState('')
+
+    const sortVeg = () => {
+        let vegRestros = restaurants.filter(res => res.info.veg === true);
+        updateFilteredRestaurants(vegRestros)
+    }
+
+    const allRestaurants = () => {
+        updateFilteredRestaurants(restaurants)
+    }
+
+    const topRestaurants = () => {
+        let topRestros = restaurants.filter(res => res?.info?.avgRating > 4.0)
+        updateFilteredRestaurants(topRestros)
+    }
+
+    const handleSearch = e => {
+        e.preventDefault();
+        let searchedRestaurants = restaurants.filter(res => res.info?.name.toLowerCase().includes(query.toLowerCase()));
+        updateFilteredRestaurants(searchedRestaurants);
+    }
+
+
   return (
     <div className="mt-7 ">
         <div className="text-center">
-            <input type="text" className="border-2 border-rose-400 px-4 py-1 active:border-rose-400 focus:outline-none" placeholder="search fav restro here" />
-            <button className="border-2 border-l-0 bg-rose-400 border-rose-400 px-3 py-1">
-                 <i class="fa-solid fa-magnifying-glass"></i>
+            <input type="text" value={query} onChange={e => setQuery(e.target.value) } className="border-2 border-rose-400 px-4 py-1 active:border-rose-400 focus:outline-none" placeholder="search fav restro here" />
+            <button onClick={handleSearch} className="border-2 border-l-0 bg-rose-400 border-rose-400 px-3 py-1">
+                 <i className="fa-solid fa-magnifying-glass"></i>
             </button>
             
-            <button className=" px-3 py-2 ml-1 mt-3 sm:mt-0 md:ml-4  font-normal bg-rose-300">
-                Top &nbsp; <i class="fa-regular fa-star"></i>
+            <button onClick={topRestaurants} className=" px-3 py-2 ml-1 mt-3 sm:mt-0 md:ml-4  font-normal bg-rose-300">
+                Top &nbsp; <i className="fa-regular fa-star"></i>
             </button>
-            <button className=" px-3 py-2 ml-1 mt-3 sm:mt-0 md:ml-4  font-normal bg-rose-400">
-                All  &nbsp; <i class="fa-solid fa-earth-americas"></i>
+            <button onClick={allRestaurants} className=" px-3 py-2 ml-1 mt-3 sm:mt-0 md:ml-4  font-normal bg-rose-400">
+                All  &nbsp; <i className="fa-solid fa-earth-americas"></i>
             </button>
-            <button className=" px-3 py-2 ml-1 mt-3 sm:mt-0 md:ml-4  font-normal bg-green-400">
-                Veg  &nbsp; <i class="fa-solid fa-leaf"></i>
+            <button onClick={sortVeg} className=" px-3 py-2 ml-1 mt-3 sm:mt-0 md:ml-4  font-normal bg-green-400">
+                Veg  &nbsp; <i className="fa-solid fa-leaf"></i>
             </button>
         </div>
     </div>
